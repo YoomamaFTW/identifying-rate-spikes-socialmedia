edd65648fad51cea5a916fcd0491c486f5d9a6eb
YoomamaFTW <andrewcw825@gmail.com>
2020-01-01 18:46:42 -0500

    Realized why I used rates in the first place * Used rates (first order derivative) so that I could avoid regression entirely.* Using rates, just compare slopes * Final commit of regression stuff. Now implementing real deal 1/1/2019

--- rateTesting/chooser.py
+++ rateTesting/chooser.py
@@ -17,6 +17,7 @@ async def main(speed, interval, num_of_tags, posts_threshold=15):
             await asyncio.sleep(min_speed)
         else:
             await asyncio.sleep(old_speed)
+        cycle_start_time = db.time
         for x in db.rates:
             if len(chosen_tags) == num_of_tags:
                 break
@@ -27,30 +28,15 @@ async def main(speed, interval, num_of_tags, posts_threshold=15):
             current_time = db.time
             for i, j in enumerate(rates):
                 data_points.append((current_time-i, j))
-            log_reg = logistic_regression(data_points)
-            lin_reg = linear_regression(data_points)
-            exp_reg = exponential_regression(data_points)
 
-            # Logic
-            if log_reg > lin_reg or log_reg > exp_reg:
-                continue
-
-            # We need to take a deeper look at the linear model to determine if it's actually linear or is there
-            # more to it that shows exponential increase.
-            numberOfPosts = db.numOfPosts[x]
-            latest_posts = 4
-            if numberOfPosts > 100:
-                latest_posts = int(latest_posts / 3)
-            latest_exp_reg = exponential_regression(data_points[:latest_posts])
-            latest_lin_reg = linear_regression(data_points[:latest_posts])
-            if latest_exp_reg > 85 and latest_exp_reg > latest_lin_reg:
-                chosen_tags.append(db.tags[x])
             """
-            TODO:
-            - We can also use logistic regression to check that it's actually trending now rather than before.
-            - Advised to use Shapiro-Wilk test if n=numofposts=2000
+            Ok, so instead of comparing three regression models, I need to constantly use an exponential regression
+            in different spots of data (overall last 250, and last 50).
+            
+            We just need to analyze it in different methods, such as coefficient of determination and residuals
             """
-        print("Cycle ", cycle, ":")
+        now_time = db.time
+        print("Cycle ", cycle, ":\nAt time ", cycle_start_time, ", but it is ", now_time, " time:")
         if len(chosen_tags) < num_of_tags:
             print("There were only ", len(chosen_tags), " out of the needed ", num_of_tags,
                   " tags that could be chosen as trending. Recommendation: choose multiple posts from the top ",
@@ -59,53 +45,3 @@ async def main(speed, interval, num_of_tags, posts_threshold=15):
         for x in chosen_tags:
             print(x, " (rate before: ", ", rate after:", ", number of posts in tag: ", ")\n")
         cycle += 1
-
-
-def calculate_score(r_squared, residual_std):
-    # return residual_std / r_squared
-    return r_squared
-
-
-def linear_regression(dp):
-    n = len(dp)
-    xs = [x[0] for x in dp]
-    ys = [y[1] for y in dp]
-    meanofxs = mean(xs)
-    meanofys = mean(ys)
-
-    # regression for residual
-    blah = 0
-    for i, j in enumerate(xs):
-        blah += i * ys[j]
-    SS_xy = blah - (n * meanofxs * meanofys)
-    blah = 0
-    for i, j in enumerate(xs):
-        blah += i**2
-    SS_xx = blah - n * meanofxs**2
-    slope = SS_xy / SS_xx
-    yint = meanofys - slope * meanofxs
-
-    # y = mx + b. To get residuals, you need expected - observed, find the stddev
-
-    # r-squared
-    sx = stdev(xs, meanofxs)
-    sy = stdev(ys, meanofys)
-    summation = 0
-    for i in dp:
-        summation += (i[0] - meanofxs) * (i[1] - meanofys)
-    r_sqr = (summation / sx / sy / (n-1)) ** 2
-    return calculate_score(r_sqr, 1)
-
-
-def exponential_regression(dp):
-    # Calculating r^2 isn't good for nonlinear, so we'll only be using the residual plot.
-    xs = [x[0] for x in dp]
-    ys = [log10(y[0]) for y in dp]
-    return 1
-
-
-def logistic_regression(dp):
-    # This is different from the other two
-    # https://statisticalhorizons.com/wp-content/uploads/GOFForLogisticRegression-Paper.pdf
-    # https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test#Calculating_the_test-statistic
-    return 1
